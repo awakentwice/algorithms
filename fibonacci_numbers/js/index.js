fbe9ac86ecb6f5505c035670a2c15fde9f490a70
Ренат Саматов <RASamatov@sberned.ru>
2016-07-31 15:52:19 +0300

    fibonacci numbers js

---
+++ fibonacci_numbers/js/index.js
@@ -0,0 +1,18 @@
+/** @function
+ * @name fib
+ * @param {Number} n
+ * @returns {Number}
+ */
+
+function fib(n) {
+  let a = 1;
+  let b = 1;
+
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return b;
+}
